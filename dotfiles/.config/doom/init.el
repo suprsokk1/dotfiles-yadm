b4ba49e9d42a2ca138c866e29f9a562188e47259
gepr <geir.monrad@gmail.com>
2023-07-27 08:38:40 +0200

    ..

--- dotfiles/.config/doom/init.el
+++ dotfiles/.config/doom/init.el
@@ -3,78 +3,56 @@
 (set (quote NOERROR) t)
 (set (quote query-all-font-backends) t)
 
-(doom!  :completion
-        company
-        vertico
+(doom! :completion
+       company
+       vertico
 
-        :ui
-        doom
-        hl-todo
-        modeline
-        ;; indent-guides
-        (popup +defaults)
+       :ui
+       doom
+       hl-todo
+       modeline
+       ;; indent-guides
+       treemacs
+       (popup +defaults)
 
-        :editor
-        multiple-cursors
-        file-templates
-        fold snippets
-        ;; (evil +everywhere)
+       :editor
+       multiple-cursors
+       file-templates
+       fold snippets
+       ;; (evil +everywhere)
 
-        :emacs
-        dired
-        undo
-        vc
+       :emacs
+       dired
+       undo
+       vc
 
-        :checkers
-        syntax
+       :checkers
+       syntax
+       (spell +flyspell)
 
-        :term
-        vterm
+       :term
+       vterm
 
-        :tools
-        lsp
-        magit
-        direnv
-        editorconfig
-        lookup
-        ansible
-        (eval +overlay)
-        (docker +lsp)
+       :tools
+       lsp
+       magit
+       direnv
+       editorconfig
+       lookup
+       ansible
+       (eval +overlay)
+       (docker +lsp)
 
-        :lang
-        data
-        emacs-lisp
-        yaml
-        (org +roam2)
-        (dart +flutter)
-        (python +lsp +pyenv +pipenv)
-        (sh +lsp)
-        (go +lsp)
-        (cc +lsp)
+       :lang
+       data
+       emacs-lisp
+       yaml
+       (org +roam2)
+       (dart +flutter)
+       (python +lsp +pyenv +pipenv)
+       (sh +lsp)
+       (go +lsp)
+       (cc +lsp)
 
-        :config
-        (default +bindings))
-
-;; (set (quote UID) (string-to-number (string-trim-right (shell-command-to-string "id -u"))))
-;; (set (quote HOME) (getenv "HOME"))
-;; (set (quote PATH) (mapconcat #'princ
-;;                              (list "/usr/bin"
-;;                                    "/usr/sbin"
-;;                                    "/bin"{
-;;                                    "/sbin"
-;;                                    "/usr/local/bin/"
-;;                                    "/usr/local/sbin/") ":"))
-;; (setq %h HOME %p PATH %u UID)
-
-;; (set (quote XDG_RUNTIME_DIR) (or (getenv "XDG_RUNTIME_DIR")
-;;                                  (format "/run/user/" %d)))
-;; (set (quote XDG_CONFIG_DIR) (or (getenv "XDG_CONFIG_DIR")
-;;                                 (format "%s/.config" HOME)))
-
-;; (setenv "PATH" (mapconcat  #'princ (list
-;;                                     (format "%s/shims" HOME)
-;;                                     (format "%s/go/bin" HOME)
-;;                                     PATH
-;;                                     (format "%s/.cargo/bin" HOME)
-;;                                     (format "%s/.local/bin" HOME)
-;;                                     (format "/home/linuxbrew/.linuxbrew" HOME))  ":"))
+       :config
+       (default +bindings))
